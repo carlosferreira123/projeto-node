d80dd182e686c1d0ec5bc94888489794d4f9645c
Carlos Alisson <carlosalisson01@gmail.com>
2025-08-28 15:43:14 -0300

    feat users test

--- src/server.js
+++ src/server.js
@@ -2,15 +2,25 @@ import  http from 'http'
 
 
 
+const users = []
+
 const server =  http.createServer((req, res) => {
 const {method, url} = req
 
  if (method === 'GET' && url === '/users') {
-  return res.end('Listagem de usuarios')
+  return res
+  .setHeader('Content-type', 'application/json')
+  .end(JSON.stringify(users))
  }
 
  if (method === 'POST' && url === '/users'){
-  return res.end('Criação de usuários')
+     users.push({
+        id: 1,
+        name: 'John Doe',
+        email: 'john.doe@example.com'
+     })
+
+    return res.end('Criação de usuários')
  }
 
  return  res.end('Hello world')
